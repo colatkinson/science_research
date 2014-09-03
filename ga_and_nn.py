774f75e977582ff98e7decf27fb0c3ae2df8021d
Colin Atkinson <colin.william.atkinson@gmail.com>
2014-09-03 17:29:14 -0400

    Refactor code

--- ga_and_nn.py
+++ ga_and_nn.py
@@ -1,184 +1,158 @@
-from __future__ import division
-from pyevolve import G1DList
-from pyevolve import GSimpleGA
-from pyevolve import GAllele
-from pyevolve import Selectors
-from pyevolve import Mutators
-from pyevolve import Initializators
-from pybrain.datasets            import ClassificationDataSet, SupervisedDataSet
-from pybrain.utilities           import percentError
-from pybrain.tools.shortcuts     import buildNetwork
+#!/bin/env python2
+"""A script to use GA to evolve MLP for financial prediction."""
+from __future__ import division, print_function
+from pyevolve import (G1DList, GSimpleGA, GAllele, Selectors, Mutators,
+                      Initializators)
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules   import SoftmaxLayer
-from pybrain.structure           import TanhLayer
-from pybrain.structure           import SigmoidLayer
-from pybrain.structure           import RecurrentNetwork
-from pybrain.structure           import LinearLayer
-from pybrain.structure           import FullConnection
-import math
+from pybrain.structure import TanhLayer, SigmoidLayer
 import pickle
 import random
 random.seed()
-import sys
 import re
-import numpy
 import matplotlib.pyplot as plt
 from decimal import Decimal
-import imp
-import timeit
-"""try:
-    imp.find_module("arac")
-    _FAST = True
-except ImportError:
-    _FAST = False"""
+
 _PREFIX = "nn_ga_"
 _FAST = False
 
 
-class FinanceNet:
-    def parse_data(self, fn, begin=1000, end=1500):
+class FinanceNet(object):
+
+    """A class to represent a financial MLP."""
+
+    def parse_data(self, filen, begin=1000, end=1500):
+        """Parse data from file name in the format from the Federal Reserve."""
         arr = []
-        with open(fn) as f:
+        with open(filen) as data_file:
             lines_old = []
-            for line in f.read().split("\n")[begin:end]:  #Split into lines
-                splt = re.split("\s+", line.lstrip())
+            for line in data_file.read().split("\n")[begin:end]:
+                splt = re.split(r"\s+", line.lstrip())
                 if len(splt) >= 2:
-                    if splt[1] != "ND":  #Remove points with no data
-                        if len(lines_old) == 0:
-                            chg = 0.5
-                        else:
-                            if float(splt[1]) > float(lines_old[0][1]):
-                                chg = 1
-                            elif float(splt[1]) < float(lines_old[0][1]):
-                                chg = 0
-                            else:
-                                chg = 0
+                    if splt[1] != "ND":  # Remove points with no data
+                        if len(lines_old) != 0:
                             if len(arr) >= 1:
-                                #print lines_old[-1]
-                                #print lines_old[-1][2]
                                 arr[-1][3] = splt[1]
                             if len(lines_old) >= 5:
-                                arr_tmp = [float(lines_old[0][1]), float(lines_old[2][1]), float(lines_old[4][1]), 0]
+                                arr_tmp = [float(lines_old[0][1]),
+                                           float(lines_old[2][1]),
+                                           float(lines_old[4][1]), 0]
                                 arr.append(arr_tmp)
                         lines_old.insert(0, splt)
-        #print len(arr)
         return arr
 
     def make_dataset(self, arr):
+        """Generate a dataset from the data."""
         data = SupervisedDataSet(3, 1)
         for i in arr:
-            #print len(i)
             if len(i) == 4:
-                #print i
                 data.addSample(i[0:3], i[3])
         return data
 
     def training(self, data):
-        n = buildNetwork(data.indim, self.hidden_nodes, data.outdim, hiddenclass=self.layer_type, bias=self.bias, fast=self.fast)
-        n.randomize()
-        t = BackpropTrainer(n, dataset=data, momentum=self.momentum, verbose=False, weightdecay=0.01)
-        t.trainUntilConvergence(maxEpochs=self.epochs)
-        #t = 0
-    
-        return (t, n)
-    
+        """Train the network on the data."""
+        net = buildNetwork(data.indim, self.hidden_nodes, data.outdim,
+                           hiddenclass=self.layer_type, bias=self.bias,
+                           fast=self.fast)
+        net.randomize()
+        trnr = BackpropTrainer(net, dataset=data, momentum=self.momentum,
+                               verbose=False, weightdecay=0.01)
+        trnr.trainUntilConvergence(maxEpochs=self.epochs)
+
+        return (trnr, net)
+
     def plot(self):
-        y = self.y
-        x = range(len(y))
-        y2 = self.y2
-        
-        plt.plot(x, y, linestyle="dashed", marker="o", color="green", label="Predicted")
-        plt.plot(x, y2, linestyle="solid", marker="o", color="red", label="Actual")
+        """Plot the results using matplotlib."""
+        y_data = self.y_data
+        x_data = range(len(y_data))
+        y2_data = self.y2_data
+
+        plt.plot(x_data, y_data, linestyle="dashed", marker="o", color="green",
+                 label="Predicted")
+        plt.plot(x_data, y2_data, linestyle="solid", marker="o", color="red",
+                 label="Actual")
         plt.ylabel("Exchange Rate (USD)")
         plt.xlabel("Day")
         plt.title("German Mark/USD Exchange Rate vs Time")
         plt.legend(loc="lower right")
         plt.show()
-    
-    def test(self, t, net):
-        t.testOnData(self.tstdata, verbose = False)
-        #d2 = self.make_dataset(self.parse_data("dat96_ge.txt", begin=2000-self.days, end=2000))
-        d2 = self.tstdata
-        arr = [net.activate(x) for x, _ in d2]
-    
-        #Now to plot the data
-        y = arr
-        #x = range(len(arr))
-        y2 = []
-        for i in d2["input"]:
-            y2.append(i[0])
-        self.y = y
-        self.y2 = y2
-        #print d2["input"]
-        """plt.plot(x, y, linestyle="dashed", marker="o", color="green", label="Predicted")
-        plt.plot(x, y2, linestyle="solid", marker="o", color="red", label="Actual")
-        plt.ylabel("Exchange Rate (USD)")
-        plt.xlabel("Day")
-        plt.title("German Mark/USD Exchange Rate vs Time")
-        plt.legend(loc="lower right")"""
-        
-        print("NMSE: %s" % self.calc_NMSE(y, y2))
-    
-        #show plot
-        #plt.show()
-        return self.calc_NMSE(y, y2)
 
+    def test(self, trnr, net):
+        """Test the net on the data, and return the NMSE."""
+        trnr.testOnData(self.tstdata, verbose=False)
+        data = self.tstdata
+        arr = [net.activate(x) for x, _ in data]
+
+        # Now to plot the data
+        y_data = arr
+        y2_data = []
+        for i in data["input"]:
+            y2_data.append(i[0])
+        self.y_data = y_data
+        self.y2_data = y2_data
+
+        nmse = self.calc_nmse(y_data, y2_data)
+
+        print("NMSE: %s" % nmse)
+
+        return nmse
 
     def load_data(self):
+        """Load data from file and split it into testing and training."""
         alldata = self.make_dataset(self.parse_data("dat96_ge.txt"))
-        tstdata, trndata = alldata.splitWithProportion( 0.25 )
-        #trndata, tstdata = alldata.splitWithProportion( 0.75 )
-        tst_len = int(len(alldata)*0.25)
-    
+        tstdata, trndata = alldata.splitWithProportion(0.25)
+
         return (alldata, tstdata, trndata)
 
-    def calc_NMSE(self, actual, predicted):
-        t = 0
-        if(len(actual) == len(predicted)):
+    def calc_nmse(self, actual, predicted):
+        """Calculate the NMSE given lists of actual and predicted."""
+        total = 0
+        if len(actual) == len(predicted):
             for i in range(0, len(actual)):
-                t += abs(actual[i]-predicted[i])
+                total += abs(actual[i]-predicted[i])
             try:
-                return t/len(actual)
+                return total/len(actual)
             except ZeroDivisionError:
-                return 1000 #arbitrary big number
+                return 1000  # arbitrary big number to prevent selection
         else:
             print("Actual and predicted arrays must have the same length")
             return -1.0
-            
+
     def calc_up_down_acc(self, actual, predicted):
-        t = 0
-        if(len(actual) == len(predicted)):
+        """Calculate the up/down accuracy given actual and predicted."""
+        total = 0
+        if len(actual) == len(predicted):
             for i in range(0, len(actual)-1):
                 delta_act = actual[i+1] - actual[i]
                 delta_pre = predicted[i+1] - predicted[i]
                 if delta_act/abs(delta_act) == delta_pre/abs(delta_pre):
-                    t += 1
+                    total += 1
             try:
-                return t/(len(actual)-1)*100
+                return total/(len(actual)-1)*100
             except ZeroDivisionError:
-                return 0 #just 0 bruh
+                return 0  # if zero division happens, just give up
         else:
             print("Actual and predicted arrays must have the same length")
             return -1.0
-    
+
     def run(self):
+        """Execute the NN (train and calculate NMSE)."""
         if self.newnet:
             self.trained, self.net = self.training(self.trndata)
-            #test(trained, data2)
-            """with open(self.fn_prefix+"_network", "w") as f:
-                pickle.dump(self.net, f)
-            with open(self.fn_prefix+"_trainer", "w") as f:
-                pickle.dump(self.trained, f)"""
         else:
             self.trained = 0
             self.net = 0
-            with open(self.fn_prefix+"_network", "r") as f:
-                self.net = pickle.load(f)
-            with open(self.fn_prefix+"_trainer", "r") as f:
-                self.trained = pickle.load(f)
+            with open(self.fn_prefix+"_network", "r") as data_file:
+                self.net = pickle.load(data_file)
+            with open(self.fn_prefix+"_trainer", "r") as data_file:
+                self.trained = pickle.load(data_file)
         self.nmse = self.test(self.trained, self.net)
-    
-    def __init__(self, prefix, newnet=True, fast=False, days=500, hidden_nodes=6, epochs=30, bias=True, momentum=0.1, layer=TanhLayer):
+
+    def __init__(self, prefix, newnet=True, fast=False, days=500,
+                 hidden_nodes=6, epochs=30, bias=True, momentum=0.1,
+                 layer=TanhLayer):
+        """Initialize the class."""
         self.alldata, self.tstdata, self.trndata = self.load_data()
         self.fn_prefix = prefix
         self.newnet = newnet
@@ -190,105 +164,105 @@ class FinanceNet:
         self.layer_type = layer
         self.fast = fast
 
+        self.trained = 0
+        self.y_data = []
+        self.y2_data = []
+        self.nmse = 0.0
+        self.net = 0
+
+
 def evolve_callback(ga_engine):
+    """Callback for each generation. Prints %% progress."""
     generation = ga_engine.getCurrentGeneration()
     if generation % 100 == 0:
-        print "Current generation: %d" % (generation,)
-        print ga_engine.getStatistics()
+        print("Current generation: %d" % (generation,))
+        print(ga_engine.getStatistics())
     return False
 
-def gen_net(chromosome, fast=False):
-    """This is just here so I only have to change the chromosome around
-    in one place"""
-    n = FinanceNet(_PREFIX, days=chromosome[0],
-                    hidden_nodes=chromosome[1],
-                    epochs=chromosome[2], bias=chromosome[3],
-                    momentum=chromosome[4], layer=chromosome[5],
-                    fast=fast)
-    return n
 
-def eval_func(chromosome):
-    score = 0.0
-    #print chromosome
-    """net = FinanceNet("nn_ga_", days=chromosome[0],
+def gen_net(chromosome, fast=False):
+    """Generate a network from the given chromosome."""
+    net = FinanceNet(_PREFIX, days=chromosome[0],
                      hidden_nodes=chromosome[1],
                      epochs=chromosome[2], bias=chromosome[3],
-                     momentum=chromosome[4], layer=chromosome[5])"""
+                     momentum=chromosome[4], layer=chromosome[5],
+                     fast=fast)
+    return net
+
+
+def eval_func(chromosome):
+    """Calculate score of chromosome."""
+    score = 0.0
     net = gen_net(chromosome, chromosome.getParam("fast"))
     net.run()
-    score = 1/net.nmse   #to counteract the fact that higher score==better
-    #print score
+    score = 1/net.nmse  # take inverse so lower is better
     return score
 
+
 def save_class(inst, prefix):
-    with open(prefix+"_class", "w") as f:
-        pickle.dump(inst, f)
+    """Pickle the given neural network instance."""
+    with open(prefix+"_class", "w") as data_file:
+        pickle.dump(inst, data_file)
     return True
 
+
 def load_class(prefix):
-    with open(prefix+"_class") as f:
-        return pickle.load(f)
+    """Load a pickled neural network class instance."""
+    with open(prefix+"_class") as data_file:
+        return pickle.load(data_file)
+
 
 def run_ga(fast):
-    alleles = GAllele.GAlleles()    #create list of all alleles
-    alleles.add(GAllele.GAlleleRange(1, 1000))  #add the allele range for the number of days, from 0-1000
-    alleles.add(GAllele.GAlleleRange(1, 20))   #add the range for number of hidden nodes
-    alleles.add(GAllele.GAlleleRange(1, 10))    #number of epochs (maybe raise later, but not tryna have like 1000 gens
-    alleles.add(GAllele.GAlleleList([True, False])) #bias
-    alleles.add(GAllele.GAlleleList([float(x * Decimal("0.1")) for x in xrange(0, 10)])) #momentum. Doing complicated stuff with Decimal to avoid error
-    #alleles.add(GAllele.GAlleleList([float(x * Decimal("0.01")) for x in xrange(0, 100)])) #weight decay
-    alleles.add(GAllele.GAlleleList([TanhLayer, SigmoidLayer]))
-    
+    """Initialize and run the genetic algorithms."""
+    alleles = GAllele.GAlleles()                    # create list of all alleles
+    alleles.add(GAllele.GAlleleRange(1, 1000))    # range for the number of days
+    alleles.add(GAllele.GAlleleRange(1, 20))            # range for hidden nodes
+    alleles.add(GAllele.GAlleleRange(1, 10))                  # number of epochs
+    alleles.add(GAllele.GAlleleList([True, False]))                       # bias
+    alleles.add(GAllele.GAlleleList([float(x * Decimal("0.1"))
+                                     for x in xrange(0, 10)]))        # momentum
+    alleles.add(GAllele.GAlleleList([TanhLayer, SigmoidLayer]))   # tanh/sigmoid
+
     genome = G1DList.G1DList(len(alleles))
     genome.setParams(allele=alleles, fast=fast)
     genome.evaluator.set(eval_func)
-    
-    #genome.evaluator.set(eval_func)
+
     genome.mutator.set(Mutators.G1DListMutatorAllele)
     genome.initializator.set(Initializators.G1DListInitializatorAllele)
-    
-    ga = GSimpleGA.GSimpleGA(genome)
-    #ga.stepCallback.set(evolve_callback)
-    #ga.setMultiProcessing(True)
-    ga.setMultiProcessing(not fast)
-    ga.selector.set(Selectors.GRouletteWheel)
-    ga.setGenerations(20)
-    
-    ga.evolve(freq_stats=0)
-    #print ga.bestIndividual()
-    #print ga.bestIndividual().genomeList
-    return ga.bestIndividual()
+
+    gen_alg = GSimpleGA.GSimpleGA(genome)
+    gen_alg.setMultiProcessing(not fast)
+    gen_alg.selector.set(Selectors.GRouletteWheel)
+    gen_alg.setGenerations(20)
+
+    gen_alg.evolve(freq_stats=0)
+    return gen_alg.bestIndividual()
+
 
 def multiprocess_run():
+    """Run with GA multiprocessing."""
     main(False)
 
+
 def arac_run():
+    """Run with accelerated NN (arac)."""
     main(True)
 
+
 def main(fast):
-    #print _FAST
+    """Main function."""
     if fast:
         print("Running in fast mode (arac)")
     else:
         print("Running in slow mode")
-    #raw_input()
-    bi = run_ga(fast)
-    print bi
-    print bi.genomeList
-    """net = FinanceNet(_PREFIX, days=chromosome[0],
-                     hidden_nodes=chromosome[1],
-                     epochs=chromosome[2], bias=chromosome[3],
-                     momentum=chromosome[4], layer=chromosome[5])"""
-    net = gen_net(bi.genomeList)
+
+    res = run_ga(fast)
+    print(res)
+    print(res.genomeList)
+    net = gen_net(res.genomeList)
     net.run()
-    #net.plot()
+    net.plot()
     save_class(net, _PREFIX)
 
 if __name__ == "__main__":
-    #_FAST = False
-    #print "Multiprocess"
-    #print timeit.Timer(lambda: multiprocess_run(), "gc.enable()").timeit(1)
-    #_FAST = True
-    #print "arac"
-    #print timeit.Timer(lambda: arac_run(), "gc.enable()").timeit(1)
     main(_FAST)
