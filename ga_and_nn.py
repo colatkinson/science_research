13d76bbe8ad47ad65b40ff1d483944d89d673732
Colin Atkinson <colin.william.atkinson@gmail.com>
2014-10-30 21:15:33 -0400

    2014-10-30 21:15

--- ga_and_nn.py
+++ ga_and_nn.py
@@ -1,4 +1,4 @@
-#!/bin/env python2
+# ga_and_nn.py
 """A script to use GA to evolve MLP for financial prediction."""
 from __future__ import division, print_function
 from pyevolve import (G1DList, GSimpleGA, GAllele, Selectors, Mutators,
@@ -13,16 +13,29 @@ random.seed()
 import re
 import matplotlib.pyplot as plt
 from decimal import Decimal
+from pprint import pprint
 
 _PREFIX = "nn_ga_"
 _FAST = False
+_FILEN = ""
+
+
+class Currency(object):
+
+    """A class to represent an individual currency for testing."""
+
+    def __init__(self, name, filen, prefix):
+        """Initialize with name, file name, and prefix."""
+        self.name = name
+        self.filen = filen
+        self.prefix = prefix
 
 
 class FinanceNet(object):
 
     """A class to represent a financial MLP."""
 
-    def parse_data(self, filen, begin=1000, end=1500):
+    def parse_data(self, filen, begin=1000, end=1500, incl=""):
         """Parse data from file name in the format from the Federal Reserve."""
         arr = []
         with open(filen) as data_file:
@@ -62,7 +75,7 @@ class FinanceNet(object):
 
         return (trnr, net)
 
-    def plot(self):
+    def plot(self, name):
         """Plot the results using matplotlib."""
         y_data = self.y_data
         x_data = range(len(y_data))
@@ -70,17 +83,17 @@ class FinanceNet(object):
 
         plt.plot(x_data, y_data, linestyle="dashed", marker="o", color="green",
                  label="Predicted")
-        plt.plot(x_data, y2_data, linestyle="solid", marker="o", color="red",
+        plt.plot(x_data, y2_data, linestyle="solid", marker="o", color="black",
                  label="Actual")
         plt.ylabel("Exchange Rate (USD)")
         plt.xlabel("Day")
-        plt.title("German Mark/USD Exchange Rate vs Time")
+        plt.title("%s/USD Exchange Rate vs Time" % name)
         plt.legend(loc="lower right")
         plt.show()
 
     def test(self, trnr, net):
-        """Test the net on the data, and return the NMSE."""
-        trnr.testOnData(self.tstdata, verbose=False)
+        """Test the net on the data, and return the MAE."""
+        trnr.testOnData(self.tstdata, verbose=True)
         data = self.tstdata
         arr = [net.activate(x) for x, _ in data]
 
@@ -92,21 +105,25 @@ class FinanceNet(object):
         self.y_data = y_data
         self.y2_data = y2_data
 
-        nmse = self.calc_nmse(y_data, y2_data)
+        mae = self.calc_mae(y_data, y2_data)
 
-        print("NMSE: %s" % nmse)
+        print("MAE: %s" % mae)
 
-        return nmse
+        up_down = self.calc_up_down_acc(y_data, y2_data)
+
+        print("Up/DownL %s" % up_down)
+
+        return mae
 
     def load_data(self):
         """Load data from file and split it into testing and training."""
-        alldata = self.make_dataset(self.parse_data("dat96_ge.txt"))
+        alldata = self.make_dataset(self.parse_data(self.data_fn))
         tstdata, trndata = alldata.splitWithProportion(0.25)
 
         return (alldata, tstdata, trndata)
 
-    def calc_nmse(self, actual, predicted):
-        """Calculate the NMSE given lists of actual and predicted."""
+    def calc_mae(self, actual, predicted):
+        """Calculate the MAE given lists of actual and predicted."""
         total = 0
         if len(actual) == len(predicted):
             for i in range(0, len(actual)):
@@ -137,7 +154,7 @@ class FinanceNet(object):
             return -1.0
 
     def run(self):
-        """Execute the NN (train and calculate NMSE)."""
+        """Execute the NN (train and calculate MAE)."""
         if self.newnet:
             self.trained, self.net = self.training(self.trndata)
         else:
@@ -147,12 +164,13 @@ class FinanceNet(object):
                 self.net = pickle.load(data_file)
             with open(self.fn_prefix+"_trainer", "r") as data_file:
                 self.trained = pickle.load(data_file)
-        self.nmse = self.test(self.trained, self.net)
+        self.mae = self.test(self.trained, self.net)
 
-    def __init__(self, prefix, newnet=True, fast=False, days=500,
+    def __init__(self, prefix, data_fn, newnet=True, fast=False, days=500,
                  hidden_nodes=6, epochs=30, bias=True, momentum=0.1,
                  layer=TanhLayer):
         """Initialize the class."""
+        self.data_fn = data_fn
         self.alldata, self.tstdata, self.trndata = self.load_data()
         self.fn_prefix = prefix
         self.newnet = newnet
@@ -167,7 +185,7 @@ class FinanceNet(object):
         self.trained = 0
         self.y_data = []
         self.y2_data = []
-        self.nmse = 0.0
+        self.mae = 0.0
         self.net = 0
 
 
@@ -180,9 +198,9 @@ def evolve_callback(ga_engine):
     return False
 
 
-def gen_net(chromosome, fast=False):
+def gen_net(chromosome, prefix=_PREFIX, fast=False):
     """Generate a network from the given chromosome."""
-    net = FinanceNet(_PREFIX, days=chromosome[0],
+    net = FinanceNet(prefix, _FILEN, days=chromosome[0],
                      hidden_nodes=chromosome[1],
                      epochs=chromosome[2], bias=chromosome[3],
                      momentum=chromosome[4], layer=chromosome[5],
@@ -193,9 +211,9 @@ def gen_net(chromosome, fast=False):
 def eval_func(chromosome):
     """Calculate score of chromosome."""
     score = 0.0
-    net = gen_net(chromosome, chromosome.getParam("fast"))
+    net = gen_net(chromosome, prefix=_PREFIX, fast=chromosome.getParam("fast"))
     net.run()
-    score = 1/net.nmse  # take inverse so lower is better
+    score = 1/net.mae  # take inverse so lower is better
     return score
 
 
@@ -214,14 +232,14 @@ def load_class(prefix):
 
 def run_ga(fast):
     """Initialize and run the genetic algorithms."""
-    alleles = GAllele.GAlleles()                    # create list of all alleles
-    alleles.add(GAllele.GAlleleRange(1, 1000))    # range for the number of days
-    alleles.add(GAllele.GAlleleRange(1, 20))            # range for hidden nodes
-    alleles.add(GAllele.GAlleleRange(1, 10))                  # number of epochs
-    alleles.add(GAllele.GAlleleList([True, False]))                       # bias
+    alleles = GAllele.GAlleles()                   # create list of all alleles
+    alleles.add(GAllele.GAlleleRange(1, 1000))   # range for the number of days
+    alleles.add(GAllele.GAlleleRange(1, 20))           # range for hidden nodes
+    alleles.add(GAllele.GAlleleRange(1, 10))                 # number of epochs
+    alleles.add(GAllele.GAlleleList([True, False]))                      # bias
     alleles.add(GAllele.GAlleleList([float(x * Decimal("0.1"))
-                                     for x in xrange(0, 10)]))        # momentum
-    alleles.add(GAllele.GAlleleList([TanhLayer, SigmoidLayer]))   # tanh/sigmoid
+                                     for x in range(0, 10)]))        # momentum
+    alleles.add(GAllele.GAlleleList([TanhLayer, SigmoidLayer]))  # tanh/sigmoid
 
     genome = G1DList.G1DList(len(alleles))
     genome.setParams(allele=alleles, fast=fast)
@@ -249,20 +267,32 @@ def arac_run():
     main(True)
 
 
-def main(fast):
+def main(fast, currency):
     """Main function."""
+    global _FILEN
     if fast:
         print("Running in fast mode (arac)")
     else:
         print("Running in slow mode")
 
+    _FILEN = currency.filen
+
+    print("Testing on %s (%s)" % (currency.name, _FILEN))
+
     res = run_ga(fast)
     print(res)
     print(res.genomeList)
-    net = gen_net(res.genomeList)
+    net = gen_net(res.genomeList, prefix=currency.prefix)
     net.run()
-    net.plot()
-    save_class(net, _PREFIX)
+    net.plot(currency.name)
+    save_class(net, currency.prefix)
 
 if __name__ == "__main__":
-    main(_FAST)
+    german_mark = Currency("German Mark", "dat96_ge.txt", "mark_nn_")
+    gbp = Currency("British Pound", "dat96_uk.txt", "gbp_nn_")
+    yen = Currency("Japanese Yen", "dat96_ja.txt", "yen_nn_")
+    # main(_FAST, yen)
+    net = load_class("yen2")
+    # pprint(vars(net))
+    net.run()
+    net.plot("Japanese Yen")
--- simulation.py
+++ simulation.py
@@ -1,8 +1,12 @@
+# simulation.py
+"""A script to test existing data with a market simulation."""
 from __future__ import division
 from ga_and_nn import *
 
+
 def simul():
-    n = load_class("nn_ga_")
+    """Run the market simulation."""
+    n = load_class("mark2")
     data = n.tstdata["input"]
     cash_init = 10000
     cash = cash_init
@@ -11,18 +15,17 @@ def simul():
     bh_cash = 10000
     bh_shares = int(bh_cash/data[0][0])
     bh_cash -= bh_shares*data[0][0]
-    print "Buy Hold: %s %s" % (bh_cash, bh_shares)
+    print("Buy Hold: %s %s" % (bh_cash, bh_shares))
     stock_val = []
     for i in data:
         stock_val.append(i[0])
     cash_arr = []
     cash_stock = []
-    
+
     for i in xrange(0, len(data)-1):
         v = n.net.activate(data[i])
         delta = v - data[i][0]
         sign = delta/abs(delta)
-        #print delta
         if sign > 0:
             if cash - data[i][0] > 0:
                 shares += int(cash/data[i][0])
@@ -33,29 +36,31 @@ def simul():
         """if cash > cash_init:
             savings += cash-cash_init
             cash = cash_init"""
-        print cash, shares
+        print(cash, shares)
         cash_arr.append(cash)
         cash_stock.append(cash+data[i][0]*shares)
     cash += shares*data[-1][0]
     shares = 0
     bh_cash += bh_shares*data[-1][0]
     bh_shares = 0
-    #print shares
-    print "\nResults"
-    print "NN: %s" % (cash+savings)
-    print "B/H: %s" % bh_cash
+    print("\nResults")
+    print("NN: %s" % (cash+savings))
+    print("B/H: %s" % bh_cash)
     plot_data(cash_arr, cash_stock, stock_val)
 
+
 def plot_data(cash, cash_shares, stock_val):
+    """Plot the simulation data."""
     x = xrange(0, len(cash))
-    plt.plot(x, cash, linestyle="dashed", marker="o", color="green", label="Cash")
-    plt.plot(x, cash_shares, linestyle="solid", marker="o", color="red", label="Cash+Shares")
-    plt.plot(x, [x*5000 for x in stock_val[:len(x)]], linestyle="solid", marker="o", color="blue", label="Stock Value (x5000)")
+    plt.plot(x, cash, linestyle="dashed", marker="o",
+             color="green", label="Cash")
+    plt.plot(x, cash_shares, linestyle="solid", marker="o",
+             color="red", label="Cash+Shares")
     plt.ylabel("Cash")
     plt.xlabel("Day")
     plt.title("German Mark/USD Exchange Rate vs Time")
     plt.legend(loc="lower right")
     plt.show()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     simul()
